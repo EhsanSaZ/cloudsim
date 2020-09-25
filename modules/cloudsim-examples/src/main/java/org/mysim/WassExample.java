3d6a23cffef55d1efbcc4bb15cf8b460723cf0f9
ehsna <e24saeedi@gmail.com>
2020-09-25 16:18:51 +0330

    Add usage of MHHBDCS_WorkflowEngine  and RankAndDeadlineDistribution strategy in WaasExample

--- modules/cloudsim-examples/src/main/java/org/mysim/WassExample.java
+++ modules/cloudsim-examples/src/main/java/org/mysim/WassExample.java
@@ -19,6 +19,7 @@ import org.cloudbus.cloudsim.container.utils.IDs;
 import org.cloudbus.cloudsim.core.CloudSim;
 import org.mysim.budgetdistribution.BudgetDistributionStrategySpareBudget;
 import org.mysim.deadlinedistribution.DeadlineDistributionSimpleUpwardRank;
+import org.mysim.deadlinedistribution.RankAndDeadlineDistributionMWHBDCSAlgorithm;
 import org.mysim.planning.MySecondPlanningAlgorithm;
 import org.mysim.planning.PlanningAlgorithmStrategy;
 import org.mysim.utils.Parameters;
@@ -101,7 +102,8 @@ public class WassExample {
             Log.setOutput(logFileStream);
 
             // create all allocations and distributions strategy
-            DeadlineDistributionSimpleUpwardRank ddDistribution = new DeadlineDistributionSimpleUpwardRank();
+//            DeadlineDistributionSimpleUpwardRank ddDistribution = new DeadlineDistributionSimpleUpwardRank();
+            RankAndDeadlineDistributionMWHBDCSAlgorithm ddDistribution = new RankAndDeadlineDistributionMWHBDCSAlgorithm();
 
             BudgetDistributionStrategySpareBudget bDistribution = new BudgetDistributionStrategySpareBudget();
 
@@ -118,7 +120,8 @@ public class WassExample {
             ContainerVmAllocationPolicy vmAllocationPolicy = new ContainerVmAllocationPolicySimple(hostList);
 
 
-            WorkflowEngine workflowEngine = new WorkflowEngine("workflow_engine");
+//            WorkflowEngine workflowEngine = new WorkflowEngine("workflow_engine");
+            MHHBDCS_WorkflowEngine workflowEngine = new MHHBDCS_WorkflowEngine("workflow_engine");
 
             int overBookingFactor = 80;
             WorkflowDatacenterBroker broker = createBroker(overBookingFactor, workflowEngine.getId());
@@ -239,6 +242,77 @@ public class WassExample {
         Log.printConcatLine("-------------------------------------------------------------------\n");
     }
 
+    private static void printStatus(MHHBDCS_WorkflowEngine workflowEngine, WorkflowDatacenterBroker broker){
+        int PSR = 0;
+        int DeadlineSuccess = 0;
+        double accumulatedWorkflowCost = 0;
+        double accumulatedWorkflowBudget = 0;
+        double accumulatedCostToBudget = 0;
+        double accumulatedMakeSpanToDeadline = 0;
+        int totalVmNumbers = broker.getVmDestroyedNumber() + broker.getVmsCreatedList().size();
+        double totalVmCost = 0;
+        double createdVmsAverageCpuUtilization = 0.0;
+        double allVmsAverageCpuUtilization = 0.0;
+
+        int totalWorkflowNumber = workflowEngine.getExecutedWorkflowList().size();
+
+        for (Workflow workflow: workflowEngine.getExecutedWorkflowList()) {
+            Log.printLine("--------------"+ workflow.getName()+ "-------------------");
+            Log.printConcatLine("Deadline:", workflow.getDeadline(), "\nMake Span:", workflow.getFinalMakeSpan() + "\n");
+            Log.printConcatLine("Budget:", workflow.getBudget(), "\nCost:", workflow.getTotalCost() + "\n");
+            accumulatedWorkflowCost +=  workflow.getTotalCost();
+            accumulatedWorkflowBudget += workflow.getBudget();
+            accumulatedCostToBudget += workflow.getTotalCost() / workflow.getBudget();
+            accumulatedMakeSpanToDeadline += workflow.getFinalMakeSpan() / workflow.getDeadline();
+
+            if (workflow.getFinalMakeSpan() <= workflow.getDeadline() && workflow.getTotalCost() <= workflow.getBudget()){
+                PSR++;
+            }
+            if (workflow.getFinalMakeSpan() <= workflow.getDeadline()){
+                DeadlineSuccess++;
+            }
+
+        }
+        for (ContainerVm vm : broker.getVmsCreatedList()){
+            CondorVM castedVm = (CondorVM) vm;
+            createdVmsAverageCpuUtilization += castedVm.getAverageCpuUtilization();
+        }
+        allVmsAverageCpuUtilization = (broker.getDestroyedVmAverageUtilization() * broker.getVmDestroyedNumber() + createdVmsAverageCpuUtilization) / totalVmNumbers;
+
+        Log.enable();
+        Log.printConcatLine("------------------------ Simulation Results ------------------------\n");
+        Log.printConcatLine("Total workflows: ", workflowEngine.getWorkflowParser().getTotalWorkflowNumbers());
+        Log.printConcatLine("PSR: ", (double)PSR *100 / totalWorkflowNumber, "%");
+        Log.printConcatLine("Deadline success:  ", (double)DeadlineSuccess  * 100 / totalWorkflowNumber , "%");
+        Log.printConcatLine("Accumulated Workflow Cost: ", accumulatedWorkflowCost);
+        Log.printConcatLine("Average Workflow Cost: ", accumulatedWorkflowCost / totalWorkflowNumber);
+        Log.printConcatLine("Accumulated Workflow Budget: ", accumulatedWorkflowBudget);
+        Log.printConcatLine("Average Workflow Budget: ", accumulatedWorkflowBudget / totalWorkflowNumber);
+        Log.printConcatLine("Average Cost to Budget:    ", (accumulatedCostToBudget / totalWorkflowNumber));
+        Log.printConcatLine("Average MakeSpan to Deadline:  ", (accumulatedMakeSpanToDeadline / totalWorkflowNumber));
+        Log.printConcatLine("TOTAL VM NUMBERS:  ", totalVmNumbers);
+        Log.printConcatLine("TOTAL Destroyed VM NUMBERS: ", broker.getVmDestroyedNumber());
+        Log.printConcatLine("TOTAL Created VM NUMBERS: ", broker.getVmsCreatedList().size(),"\n");
+        Log.printConcatLine("ALl VMs Average Utilization: ", allVmsAverageCpuUtilization * 100,"%\n");
+        Log.printConcatLine("------------------------ Static Experiment Parameters ------------------------\n");
+        Log.printConcatLine("Packing_VM_SELECTION_TYPE: ", Parameters.Packing_VM_SELECTION_TYPE);
+        Log.printConcatLine("R_T_Q_SCHEDULING_INTERVAL: ", Parameters.R_T_Q_SCHEDULING_INTERVAL);
+        Log.printConcatLine("MONITORING_INTERVAL: ", Parameters.MONITORING_INTERVAL);
+        Log.printConcatLine("CONTAINER_VM_SCHEDULING_INTERVAL: ", Parameters.CONTAINER_VM_SCHEDULING_INTERVAL);
+        Log.printConcatLine("VM_THRESHOLD_FOR_SHUTDOWN: ", Parameters.VM_THRESH_HOLD_FOR_SHUTDOWN);
+        Log.printConcatLine("CHECK_FINISHED_STATUS_DELAY: ", Parameters.CHECK_FINISHED_STATUS_DELAY, "\n");
+        Log.printConcatLine("VM_PROVISIONING_DELAY: ", Parameters.VM_PROVISIONING_DELAY);
+        Log.printConcatLine("VM_DESTROY_DELAY: ", Parameters.VM_DESTROY_DELAY);
+        Log.printConcatLine("CONTAINER_PROVISIONING_DELAY: ", Parameters.CONTAINER_PROVISIONING_DELAY);
+        Log.printConcatLine("CONTAINER_DESTROY_DELAY: ", Parameters.CONTAINER_DESTROY_DELAY, "\n");
+        Log.printConcatLine("CPU_DEGRADATION: ", "MEAN: ", Parameters.CPU_DEGRADATION.getMean(), ", SD: ", Parameters.CPU_DEGRADATION.getStandardDeviation());
+        Log.printConcatLine("BW_DEGRADATION: ", "MEAN: ", Parameters.BW_DEGRADATION.getMean(), ", SD: ", Parameters.BW_DEGRADATION.getStandardDeviation());
+        Log.printConcatLine("BILLING_PERIOD: ", Parameters.BILLING_PERIOD);
+        Log.printConcatLine("ALPHA_DEADLINE_FACTOR: ", Parameters.ALPHA_DEADLINE_FACTOR);
+        Log.printConcatLine("BETA_BUDGET_FACTOR: ", Parameters.BETA_BUDGET_FACTOR);
+        Log.printConcatLine("ARRIVAL_RATE: ", Parameters.ARRIVAL_RATE);
+        Log.printConcatLine("-------------------------------------------------------------------\n");
+    }
     private static WorkflowDatacenterBroker createBroker(int overBookingFactor, int workflowEngineId) {
 
         WorkflowDatacenterBroker broker = null;
