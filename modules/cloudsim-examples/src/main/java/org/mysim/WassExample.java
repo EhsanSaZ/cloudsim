ce316a02c178a5c9c3af8e31764b590bee81c486
ehsna <e24saeedi@gmail.com>
2020-10-08 11:49:59 +0330

    Add usage of EPSM planning algorithms in  WassExample

--- modules/cloudsim-examples/src/main/java/org/mysim/WassExample.java
+++ modules/cloudsim-examples/src/main/java/org/mysim/WassExample.java
@@ -19,11 +19,9 @@ import org.cloudbus.cloudsim.container.utils.IDs;
 import org.cloudbus.cloudsim.core.CloudSim;
 import org.mysim.budgetdistribution.BudgetDistributionStrategySpareBudget;
 import org.mysim.deadlinedistribution.DeadlineDistributionSimpleUpwardRank;
+import org.mysim.deadlinedistribution.EPSMDeadlineDistributionAlgorithm;
 import org.mysim.deadlinedistribution.RankAndDeadlineDistributionMWHBDCSAlgorithm;
-import org.mysim.planning.MWHBDCS_PlanningAlgorithm;
-import org.mysim.planning.MySecondPlanningAlgorithm;
-import org.mysim.planning.MyThirdPlanningAlgorithm;
-import org.mysim.planning.PlanningAlgorithmStrategy;
+import org.mysim.planning.*;
 import org.mysim.utils.Parameters;
 import org.mysim.utils.ReplicaCatalog;
 import org.mysim.utils.WorkflowParser;
@@ -106,7 +104,8 @@ public class WassExample {
             // DIFFERENT ALGORITHMS
             // create all allocations and distributions strategy
 //            DeadlineDistributionSimpleUpwardRank ddDistribution = new DeadlineDistributionSimpleUpwardRank();
-            RankAndDeadlineDistributionMWHBDCSAlgorithm ddDistribution = new RankAndDeadlineDistributionMWHBDCSAlgorithm();
+//            RankAndDeadlineDistributionMWHBDCSAlgorithm ddDistribution = new RankAndDeadlineDistributionMWHBDCSAlgorithm();
+            EPSMDeadlineDistributionAlgorithm ddDistribution = new EPSMDeadlineDistributionAlgorithm();
 
             // DIFFERENT ALGORITHMS
             BudgetDistributionStrategySpareBudget bDistribution = new BudgetDistributionStrategySpareBudget();
@@ -124,14 +123,15 @@ public class WassExample {
             ContainerVmAllocationPolicy vmAllocationPolicy = new ContainerVmAllocationPolicySimple(hostList);
 
             // DIFFERENT ALGORITHMS
-//            WorkflowEngine workflowEngine = new WorkflowEngine("workflow_engine");
+            WorkflowEngine workflowEngine = new WorkflowEngine("workflow_engine");
 //            Second_WorkflowEngine workflowEngine = new Second_WorkflowEngine("Second_workflow_engine");
-            MHHBDCS_WorkflowEngine workflowEngine = new MHHBDCS_WorkflowEngine("MHHBDCS_workflow_engine");
+//            MHHBDCS_WorkflowEngine workflowEngine = new MHHBDCS_WorkflowEngine("MHHBDCS_workflow_engine");
 
 //            PlanningAlgorithmStrategy myPlanner= new MyPlanningAlgorithm();
 //            PlanningAlgorithmStrategy myPlanner = new MySecondPlanningAlgorithm();
 //            PlanningAlgorithmStrategy myPlanner = new MyThirdPlanningAlgorithm();
-            PlanningAlgorithmStrategy myPlanner = new MWHBDCS_PlanningAlgorithm();
+//            PlanningAlgorithmStrategy myPlanner = new MWHBDCS_PlanningAlgorithm();
+            PlanningAlgorithmStrategy myPlanner = new EPSMPlanningAlgorithm();
 
 
             int overBookingFactor = 80;
